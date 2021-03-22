b552f3fc4e8921c1cb65ae9dde1a4a9877f5729e
test <test@example.com>
2021-03-22 16:59:26 +0000

    InvalidFilterException: add column & value fields

--- src/main/java/lab1/InvalidFilterException.java
+++ src/main/java/lab1/InvalidFilterException.java
@@ -1,4 +1,22 @@
 package lab1;
 
 public class InvalidFilterException extends Exception {
+
+    private final String filteredColumn;
+    private final Object invalidFilterValue;
+
+    public InvalidFilterException(String filteredColumn, Object invalidFilterValue) {
+        super("Invalid '" + filteredColumn + "' filter : " + invalidFilterValue);
+
+        this.filteredColumn = filteredColumn;
+        this.invalidFilterValue = invalidFilterValue;
+    }
+
+    public String getFilteredColumn() {
+        return filteredColumn;
+    }
+
+    public Object getInvalidFilterValue() {
+        return invalidFilterValue;
+    }
 }
